b7f1835724ccd5699521b7aeb2a4304f927b65f5
Yury Gorishniy <strausmg@gmail.com>
2020-10-12 00:37:41 +0300

    add types.{optionals, Part}

--- air/__init__.py
+++ air/__init__.py
@@ -4,3 +4,4 @@ __version__ = '0.0.5'
 from .experiment import *  # noqa
 from .io import *  # noqa
 from .task import *  # noqa
+from .types import *  # noqa
--- air/types.py
+++ air/types.py
@@ -1,7 +1,9 @@
-__all__ = ['PathLike', 'Recursive', 'JSON']
+__all__ = ['PathLike', 'Recursive', 'JSON', 'Part', 'optionals']
 
+import dataclasses as dc
+import enum
 from pathlib import Path
-from typing import Any, Dict, List, Mapping, Tuple, TypeVar, Union
+from typing import Any, Dict, List, Mapping, Optional, Tuple, TypeVar, Union
 
 PathLike = Union[Path, bytes, str]
 
@@ -40,3 +42,61 @@ JSON = Union[None, bool, int, float, str, List['JSON'], Mapping[str, 'JSON']]  #
         [0, 1.0]
         {'a': [0, 1.0], 'b': False, 'c': 'abc'}
 """
+
+
+class Part(enum.Enum):
+    TRAIN = 'train'
+    VAL = 'val'
+    TEST = 'test'
+
+    @property
+    def is_train(self) -> bool:
+        return self == Part.TRAIN
+
+    @property
+    def is_val(self) -> bool:
+        return self == Part.VAL
+
+    @property
+    def is_test(self) -> bool:
+        return self == Part.TEST
+
+
+def optionals(cls: type) -> type:
+    """Make all fields Optional with the default value None."""
+    assert dc.is_dataclass(cls)
+    fields = []
+    for x in dc.fields(cls):
+        # https://docs.python.org/3/library/dataclasses.html#dataclasses.Field
+        kwargs = {
+            k: getattr(x, k)
+            for k in [
+                'default',
+                'default_factory',
+                'init',
+                'repr',
+                'hash',
+                'compare',
+                'metadata',
+            ]
+        }
+        if (
+            isinstance(kwargs['default'], dc._MISSING_TYPE)
+            and isinstance(kwargs['default_factory'], dc._MISSING_TYPE)
+        ):
+            kwargs['default'] = None
+        type_ = (
+            x.type
+            if getattr(x.type, '_name', None) == 'Optional'
+            else Optional[x.type]
+        )
+        fields.append((x.name, type_, dc.field(**kwargs)))
+    return dc.make_dataclass(
+        cls.__name__,
+        fields,
+        **{
+            k: getattr(cls.__dataclass_params__, k)
+            for k in dir(cls.__dataclass_params__)
+            if not k.startswith('_')
+        }
+    )
